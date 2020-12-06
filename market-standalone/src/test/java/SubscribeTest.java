6184a75de12a9fa06bb9c05fba019758961c8077
lyqingye <1316515719@qq.com>
2020-12-06 20:42:21 +0800

    feat: 定义配置

--- market-standalone/src/test/java/SubscribeTest.java
+++ market-standalone/src/test/java/SubscribeTest.java
@@ -1,43 +1,32 @@
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
-import com.hazelcast.map.EntryBackupProcessor;
-import com.hazelcast.map.EntryProcessor;
-import com.market.common.utils.GZIPUtils;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.WebSocket;
+import io.vertx.core.json.JsonObject;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 public class SubscribeTest {
 
     public static void main(String[] args) {
-        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
-        IMap<Object, Object> map = hazelcastInstance.getMap("sfaf");
+        Vertx vertx = Vertx.vertx();
 
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i++) {
-            String lock = UUID.randomUUID().toString();
-            map.executeOnKey(lock, new EntryProcessor() {
-                @Override
-                public Object process(Map.Entry entry) {
-                    return null;
-                }
 
-                @Override
-                public EntryBackupProcessor getBackupProcessor() {
-                    return null;
-                }
-            });
-            map.tryLock(lock);
-            map.put(lock, UUID.randomUUID().toString());
-            map.unlock(lock);
-        }
-        long end = System.currentTimeMillis();
-        System.out.println(TimeUnit.MILLISECONDS.toSeconds(end - start));
+        ConfigStoreOptions fileStore = new ConfigStoreOptions()
+                .setType("file")
+                .setFormat("yaml")
+                .setConfig(new JsonObject().put("path", "config.yaml"));
+
+        ConfigStoreOptions sysPropsStore = new ConfigStoreOptions().setType("sys");
+
+
+        ConfigRetrieverOptions options = new ConfigRetrieverOptions()
+                .addStore(fileStore);
+        ConfigRetriever retriever = ConfigRetriever.create(vertx,options);
+
+        retriever.getConfig(ar -> {
+            if (ar.succeeded()) {
+                System.out.println(ar.result().encodePrettily());
+            }
+        });
     }
 }
