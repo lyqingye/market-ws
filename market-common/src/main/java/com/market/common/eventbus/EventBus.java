ab9f5355af82fc471e3d2eb8b83335d62c4c4720
lyqingye <1316515719@qq.com>
2020-12-18 13:44:54 +0800

    feat: 事件总线增加future 方法

--- market-common/src/main/java/com/market/common/eventbus/EventBus.java
+++ market-common/src/main/java/com/market/common/eventbus/EventBus.java
@@ -1,6 +1,7 @@
 package com.market.common.eventbus;
 
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 
@@ -18,6 +19,29 @@ public interface EventBus {
     void publish(String topic, Object message, Handler<AsyncResult<Void>> handler);
 
     /**
+     * 发送消息并且忽略返回值
+     *
+     * @param topic topic
+     * @param message 消息
+     */
+    default void publishIgnoreRs(String topic,Object message) {
+        publish(topic,message,ignored -> {});
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param topic   主题
+     * @param message 消息
+     * @return  future
+     */
+    default Future<Void> publish(String topic, Object message) {
+        Promise<Void> promise = Promise.promise();
+        publish(topic,message,promise);
+        return promise.future();
+    }
+
+    /**
      * 订阅主题
      *
      * @param topic    主题
@@ -33,8 +57,10 @@ public interface EventBus {
      * @param consumer 消费者
      * @return promise
      */
-    default Promise<String> subscribe(String topic, Consumer<Object> consumer) {
-        return null;
+    default Future<String> subscribe(String topic, Consumer<Object> consumer) {
+        Promise<String> promise = Promise.promise();
+        subscribe(topic, consumer,promise);
+        return promise.future();
     }
 
     /**
