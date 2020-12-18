b39d8f67b16d35ac509faa407c2675af7a34c39f
lyqingye <1316515719@qq.com>
2020-12-18 13:44:13 +0800

    feat: 配置服务实现增加future接口

--- market-repository/src/main/java/com/market/repository/impl/ConfigServiceImpl.java
+++ market-repository/src/main/java/com/market/repository/impl/ConfigServiceImpl.java
@@ -3,6 +3,7 @@ package com.market.repository.impl;
 import com.market.common.def.CacheKey;
 import com.market.common.service.config.ConfigService;
 import com.market.common.service.config.dto.Mapping;
+import com.market.common.utils.VertxUtil;
 import com.market.repository.repository.ConfigRepository;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
@@ -12,6 +13,7 @@ import io.vertx.core.shareddata.AsyncMap;
 import io.vertx.core.shareddata.SharedData;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ConfigServiceImpl implements ConfigService {
@@ -30,53 +32,43 @@ public class ConfigServiceImpl implements ConfigService {
      */
     private ConfigRepository repo;
 
+    private ConfigServiceImpl() {}
+
     /**
-     * 同步方式创建代理
+     * 创建配置实例
      *
-     * @param vertx 实例
+     * @param vertx      vertx
+     * @param repository 配置仓库
+     * @return future
      */
-    public ConfigServiceImpl(ConfigRepository repository, Vertx vertx, Handler<AsyncResult<Void>> handler) {
-        this.repo = repository;
-        final SharedData sd = vertx.sharedData();
-        sd.getAsyncMap(CacheKey.SYMBOL_CUSTOM_TO_GENERIC.name(), ar -> {
-            if (ar.succeeded()) {
-                c2gMappings = ar.result();
-                repository.c2gMappings(c2gMpRs -> {
-                    if (c2gMpRs.succeeded()) {
-                        for (Mapping mapping : c2gMpRs.result()) {
-                            c2gMappings.put(mapping.getSource(), mapping.getTarget(), ignored -> {
-                            });
-                        }
-                        sd.getAsyncMap(CacheKey.SYMBOL_GENERIC_TO_CUSTOM.name(), ar2 -> {
-                            if (ar2.succeeded()) {
-                                g2cMappings = ar2.result();
-                                repository.g2cMappings(g2cMpRs -> {
-                                    if (g2cMpRs.succeeded()) {
-                                        for (Mapping mapping : g2cMpRs.result()) {
-                                            g2cMappings.put(mapping.getSource(), mapping.getTarget(), ignored -> {
-                                            });
-                                        }
-                                        handler.handle(Future.succeededFuture());
-                                    } else {
-                                        handler.handle(Future.failedFuture(g2cMpRs.cause()));
-                                    }
-                                });
-                            } else {
-                                ar2.cause().printStackTrace();
-                                handler.handle(Future.failedFuture(ar2.cause()));
-                            }
-                        });
-                    } else {
-                        handler.handle(Future.failedFuture(c2gMpRs.cause()));
-                    }
-                });
-            } else {
-                ar.cause().printStackTrace();
-                handler.handle(Future.failedFuture(ar.cause()));
-            }
-        });
-
-
+    public static Future<ConfigService> create(Vertx vertx, ConfigRepository repository) {
+        ConfigServiceImpl impl = new ConfigServiceImpl();
+        impl.repo = Objects.requireNonNull(repository);
+        return VertxUtil.getAsyncMap(vertx, CacheKey.SYMBOL_CUSTOM_TO_GENERIC.name())
+                        .compose(c2gMap -> {
+                            impl.c2gMappings = c2gMap;
+                            return repository.c2gMappings()
+                                             .compose(mappings -> {
+                                                 for (Mapping mapping : mappings) {
+                                                     impl.c2gMappings.put(mapping.getSource(), mapping.getTarget(), ignored -> {
+                                                     });
+                                                 }
+                                                 return Future.succeededFuture();
+                                             });
+                        })
+                        .compose(ignored -> VertxUtil.getAsyncMap(vertx, CacheKey.SYMBOL_GENERIC_TO_CUSTOM.name()))
+                        .compose(g2cMap -> {
+                            impl.g2cMappings = g2cMap;
+                            return repository.g2cMappings()
+                                             .compose(mappings -> {
+                                                 for (Mapping mapping : mappings) {
+                                                     impl.g2cMappings.put(mapping.getSource(), mapping.getTarget(), ignored -> {
+                                                     });
+                                                 }
+                                                 return Future.succeededFuture();
+                                             });
+                        })
+                        .compose(ignored -> Future.succeededFuture(impl));
     }
 
     /**
@@ -92,7 +84,8 @@ public class ConfigServiceImpl implements ConfigService {
             if (store.succeeded()) {
                 c2gMappings.put(custom, generic, handler);
                 // 更新 g2c
-                this.putG2C(generic,custom, ignored -> {});
+                this.putG2C(generic, custom, ignored -> {
+                });
             } else {
                 handler.handle(Future.failedFuture(store.cause()));
             }
@@ -207,10 +200,10 @@ public class ConfigServiceImpl implements ConfigService {
         map.entries(ar -> {
             if (ar.succeeded()) {
                 final List<Mapping> mappings = ar.result()
-                        .entrySet()
-                        .stream()
-                        .map(Mapping::new)
-                        .collect(Collectors.toList());
+                                                 .entrySet()
+                                                 .stream()
+                                                 .map(Mapping::new)
+                                                 .collect(Collectors.toList());
                 handler.handle(Future.succeededFuture(mappings));
             } else {
                 handler.handle(Future.failedFuture(ar.cause()));
