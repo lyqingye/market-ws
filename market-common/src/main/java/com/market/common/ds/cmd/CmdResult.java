754530a762e92c7820becd3645bbe1c2a7f5c1ef
MoyunRZ <969135622@qq.com>
2021-04-18 00:48:19 +0800

    fix: 修正依赖和编译警告

--- market-common/src/main/java/com/market/common/ds/cmd/CmdResult.java
+++ market-common/src/main/java/com/market/common/ds/cmd/CmdResult.java
@@ -1,10 +1,12 @@
 package com.market.common.ds.cmd;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.concurrent.CompletableFuture;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class CmdResult<T> extends CompletableFuture<T> {
     private boolean success;
 }
